b4a329f5ac15f90f4d1e092473ba7f80bff0263e
honwen.chan <honwen.chan@gmail.com>
2021-07-30 22:41:00 +0800

    asn: more api

--- cip/asn.go
+++ cip/asn.go
@@ -1,6 +1,7 @@
 package cip
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"log"
@@ -11,7 +12,10 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-const api = `http://ip.bczs.net/AS`
+var apis = []string{
+	`http://ip.bczs.net/AS`,
+	`https://api.hackertarget.com/aslookup/?q=AS`,
+}
 
 const (
 	ASN_TENCENT_CN = 132203
@@ -24,19 +28,58 @@ const (
 	ASN_MICROSOFT  = 8075
 )
 
+func scanCIDR(content string) (ips []string) {
+	regx := regexp.MustCompile(RegxIPv4)
+
+	if strings.Contains(content, "<html") && strings.Contains(content, "<code") {
+		doc, err := goquery.NewDocumentFromReader(bytes.NewReader([]byte(content)))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		doc.Find("code").Each(func(i int, s *goquery.Selection) {
+			text := strings.TrimSpace(s.Text())
+			if regx.MatchString(strings.Split(text, `/`)[0]) {
+				ips = append(ips, text)
+			}
+		})
+	} else {
+		sc := bufio.NewScanner(strings.NewReader(content))
+		for sc.Scan() {
+			text := sc.Text()
+			if regx.MatchString(strings.Split(text, `/`)[0]) {
+				ips = append(ips, text)
+			}
+		}
+	}
+	return ips
+}
+
 func IPsOfASN(id int) (ips []string) {
-	body := wGet(api+fmt.Sprint(id), 30*time.Second)
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader([]byte(body)))
-	if err != nil {
-		log.Fatal(err)
+	var (
+		length = len(apis)
+		cchan  = make(chan []string, length)
+	)
+	for _, api := range apis {
+		go func(api string) {
+			body := wGet(api+fmt.Sprint(id), 10*time.Second)
+			cchan <- scanCIDR(body)
+		}(api)
 	}
 
-	regx := regexp.MustCompile(RegxIPv4)
-	doc.Find("code").Each(func(i int, s *goquery.Selection) {
-		text := strings.TrimSpace(s.Text())
-		if regx.MatchString(strings.Split(text, `/`)[0]) {
-			ips = append(ips, text)
+	ipMap := map[string]bool{}
+	for i := 0; i < length; i++ {
+		for _, v := range <-cchan {
+			ipMap[v] = true
 		}
-	})
+	}
+
+	ips = make([]string, len(ipMap))
+	i := 0
+	for k := range ipMap {
+		ips[i] = k
+		i++
+	}
+
 	return
 }
